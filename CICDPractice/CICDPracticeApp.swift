007299504442a1b955f685773f99c1f31ca4110d
Arjunan M <m.arjunan371@gmail.com>
2024-11-11 12:54:10 +0530

    Initial Commit

---
+++ CICDPractice/CICDPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDPracticeApp.swift
+//  CICDPractice
+//
+//  Created by Arjunan on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
